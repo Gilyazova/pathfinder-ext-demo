681daeafe5ac3501872a24dc9f8b50f4fefbf3df
Denis Larionov <fairness@mail.ru>
2012-08-15 11:36:46 +0400

    Начал добавлять данные российских паспортов

--- app/models/identity_card.rb
+++ app/models/identity_card.rb
@@ -3,4 +3,18 @@ class IdentityCard < ActiveRecord::Base
   belongs_to :extended_by, :polymorphic => true, :touch => true
 
   attr_accessible :person
+
+  attr_accessible :serie, :number
+  #
+  # Тип докуметна удостоверяющего личность
+  #
+  attr_accessible :type
+  #
+  # Дата выдачи, Код подразделения, Кем выдан, Причина выдачи документа удостоверяющего личность,
+  # Статус документа удостоверяющего личность, 
+  #
+  attr_accessible :issue_date, :issuer_code, :issuer, :reason, :state
+  #
+  # Дата утраты, Дата начала действия, Дата окончания действия
+  #
 end
--- app/models/russian_passport.rb
+++ app/models/russian_passport.rb
@@ -1,5 +1,5 @@
 class RussianPassport < ActiveRecord::Base
   has_one :identity_card, :as => :extended_by, :dependent => :destroy
 
-  attr_accessible :first_name, :last_name, :middle_name, :birth_date
+  attr_accessible :first_name, :last_name, :middle_name, :birth_date, :sex
 end
